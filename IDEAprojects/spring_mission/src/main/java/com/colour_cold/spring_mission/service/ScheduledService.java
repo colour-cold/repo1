7488ab83a45b7f3d64d04d64984e38172cc5ca2b
colour-cold <695721929@qq.com>
2020-03-24 21:28:51 +0800

    javase练习笔记与部分springboot

--- IDEAprojects/spring_mission/src/main/java/com/colour_cold/spring_mission/service/ScheduledService.java
+++ IDEAprojects/spring_mission/src/main/java/com/colour_cold/spring_mission/service/ScheduledService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class ScheduledService {
 
     //在一个特定时间执行这个方法
-    @Scheduled(cron = "0 ")
+    @Scheduled(cron = "0/2 * * * * *")
     public void hello() {
         System.out.println("hello,你被执行了");
     }
